c7c2a01026f8d30fe62c955068f11b226e96389f
jaka <jaka@live.jp>
2018-03-09 13:09:39 +0100

    Added source

---
+++ README.md
@@ -0,0 +1,14 @@
+usbtemp-csharp
+===
+
+Source code for `Thermometer` class is located in file `usbtemp.cs` (namespace `usbtemp`).
+
+The class provides the following functions.
+```
+	void Open(string)
+	void Close(void);
+	byte[] Rom(void);
+	float Temperature(void);
+```
+
+This software is in alpha stage, contributions are welcome.
---
+++ usbtemp-demo/main.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            byte[] rom;
+            float temperature;
+            usbtemp.Thermometer thermometer;
+            string hex;
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please specify serial port!");
+                goto exit;
+            }
+
+            thermometer = new usbtemp.Thermometer();
+
+            try
+            {
+                thermometer.Open(args[0]);
+            }
+            catch (System.IO.IOException e)
+            {
+                Console.WriteLine(e.Message);
+                goto exit;
+            }
+
+            try
+            {
+                rom = thermometer.Rom();
+                temperature = thermometer.Temperature();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                goto exit;
+            }
+            finally
+            {
+                thermometer.Close();
+            }
+
+            hex = BitConverter.ToString(rom).Replace("-", string.Empty);
+            Console.WriteLine($"Serial: {hex}\nTemperature: {temperature:0.00}");
+
+            exit:
+            Console.ReadKey();
+        }
+    }
+}
